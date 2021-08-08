b0aa1ec9786c9b6c5c617593a9b1abd54295d116
Ricardo <rferromoreno@gmail.com>
2021-08-08 01:03:49 -0300

    Add role widget. Setup configurations

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const { Client } = require("discord.js");
+const keepAlive = require('./server');
 
 const client = new Client();
 
 require("./core/loadWidgetListeners")(client);
 
+keepAlive();
 client.login(process.env.DISCORD_BOT_TOKEN);
